9720e2241d8af345d87aa9dce2249c941acd67c5
Alex <1737025@etudiant.cegepvicto.ca>
2018-02-26 14:47:27 -0500

    Allo
    Allo

---
+++ M/m.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from gpiozero import LED
+from time import sleep
+
+led = LED(17)
+
+led.on()
+sleep(2)
+led.off()
+sleep(1)
+led.on()
+sleep(2)
+led.off()
---
+++ Q/q.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from gpiozero import LED
+from time import sleep
+
+led = LED(17)
+led.on()
+sleep(2)
+led.off()
+sleep(1)
+led.on()
+sleep(2)
+led.off()
+sleep(1)
+led.on()
+sleep(1)
+led.off()
+sleep(1)
+led.on()
+sleep(2)
+led.off()
